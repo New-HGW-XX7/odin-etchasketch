e49504d9e4edd5be3ccd7ee462d6974f80a7d485
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-06-11 11:07:40 +0200

    Add reset button

--- script.js
+++ script.js
@@ -5,9 +5,18 @@ const container = document.querySelector('.container');
 for(let i = 0; i < 8 * 8; i++) {
     const div = document.createElement('div');
 
+
     div.addEventListener('mouseenter', () => {
         div.style.backgroundColor = 'red';
     })
 
     container.appendChild(div);
 }
+
+const resetbtn = document.querySelector('.reset');
+resetbtn.addEventListener('click', () => {
+    const divGroup = document.querySelector('.container').childNodes;
+    for(divElement of divGroup) {
+        divElement.style.backgroundColor = 'grey';
+    }
+})
