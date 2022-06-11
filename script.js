9cb578d875a7a62d8997d69618f08cdb7ce4bc88
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-06-11 10:40:36 +0200

    Generate grid with preset number of squares

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // console.log('Test');
 
 const container = document.querySelector('.container');
-const div = document.createElement('div');
 
-div.textContent = 'Text';
-container.appendChild(div);
+for(let i = 0; i < 8 * 8; i++) {
+    const div = document.createElement('div');
+   // div.textContent = i;
+    container.appendChild(div);
+}
