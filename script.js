afbe0a6139f535840a6370c481a12a3701d70319
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-06-11 12:49:40 +0200

    Add grid removal

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
+let gridSize;
+
 const container = document.querySelector('.container');
-
-/* for(let i = 0; i < 8 * 8; i++) {
-    const div = document.createElement('div');
-
-
-    div.addEventListener('mouseenter', () => {
-        div.style.backgroundColor = 'red';
-    })
-
-    container.appendChild(div);
-} */
 
 const resetbtn = document.querySelector('.reset');
 resetbtn.addEventListener('click', () => {
@@ -20,11 +11,11 @@ resetbtn.addEventListener('click', () => {
     });
 })
 
-for(let x = 0; x < 8; x++) {
+for(let x = 0; x < 16; x++) {
     const innerContainer = document.createElement('div');
     innerContainer.classList.add('innerContainer');
     container.appendChild(innerContainer);
-        for(let y = 0; y < 8; y++) {
+        for(let y = 0; y < 16; y++) {
             const innerDiv = document.createElement('div');
             innerDiv.classList.add('innerDiv');
             innerContainer.appendChild(innerDiv);
@@ -35,3 +26,10 @@ for(let x = 0; x < 8; x++) {
         }
 }
 
+const selectbtn = document.querySelector('.select-size');
+selectbtn.addEventListener('click', () => {
+    while(container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
+    }
+
+});
